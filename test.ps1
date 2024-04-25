11190570fa7d74804ae1eeb4ea860c5ceedeb4b3
Amrendra Kumar <amarendra11193@gmail.com>
2024-04-25 16:13:49 +0530

    new file

---
+++ test.ps1
@@ -0,0 +1,6 @@
+# test script
+$path = C:\Temp
+$pathnew = C:\Temp\windows2022_march.iso
+
+# The test is to find how to update a particular line in this code and commit it to the git repo
+# in automated way using PowerShell.
