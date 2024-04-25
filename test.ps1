6e79118330e302e5c5a57c0d25f33cb144519ff7
amarendra11193 <55204064+amarendra11193@users.noreply.github.com>
2024-04-25 16:44:13 +0530

    Update test.ps1
    adding line

--- test.ps1
+++ test.ps1
@@ -2,5 +2,7 @@
 $path = C:\Temp
 $pathnew = C:\Temp\windows2022_march.iso
 
+# adding another line
+
 # The test is to find how to update a particular line in this code and commit it to the git repo
 # in automated way using PowerShell.
